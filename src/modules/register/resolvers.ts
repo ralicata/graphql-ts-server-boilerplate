feb7454bc736bdd83f430ea598245c54d33ae192
Roberto Alicata <roberto.alicata@gmail.com>
2018-08-12 01:09:55 +0200

    GraphQL Schema Stitching

--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -1,12 +1,11 @@
 import * as bcrypt from 'bcryptjs';
-import { ResolverMap } from './types/graphql-utils';
-import { GQL } from './types/schema';
-import { User } from './entity/User';
+import { ResolverMap } from '../../types/graphql-utils.d';
+import { GQL } from '../../types/schema';
+import { User } from '../../entity/User';
 
 export const resolvers: ResolverMap = {
   Query: {
-    hello: (_: any, { name }: GQL.IHelloOnQueryArguments) =>
-      `Bye ${name || 'World'}`
+    bye: () => `Bye`
   },
   Mutation: {
     register: async (
