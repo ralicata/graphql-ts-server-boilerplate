af3799aa286f1cd495d531a07475abf1c0cf3929
Roberto Alicata <roberto.alicata@gmail.com>
2018-08-11 23:13:21 +0200

    Add Mutation User#register

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,5 +1,7 @@
+import * as bcrypt from 'bcryptjs';
 import { ResolverMap } from './types/graphql-utils';
 import { GQL } from './types/schema';
+import { User } from './entity/User';
 
 export const resolvers: ResolverMap = {
   Query: {
@@ -7,10 +9,16 @@ export const resolvers: ResolverMap = {
       `Bye ${name || 'World'}`
   },
   Mutation: {
-    register: (
+    register: async (
       _: any,
       { email, password }: GQL.IRegisterOnMutationArguments
     ) => {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = User.create({
+        email,
+        password: hashedPassword
+      });
+      await user.save();
       return (email = password);
     }
   }
